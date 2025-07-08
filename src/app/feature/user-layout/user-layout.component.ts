72acb8e718c161283b71eb5b3c73f5206b34c9c5
asaadfarooq1 <asad.farooq@logicps.com>
2025-07-08 14:59:39 +0500

    add

--- src/app/feature/user-layout/user-layout.component.ts
+++ src/app/feature/user-layout/user-layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { AdminSidebarComponent } from '../../shared/components/admin-sidebar/admin-sidebar.component';
+import { UserSidebarComponent } from '../../shared/components/user-sidebar/user-sidebar.component';
 
 @Component({
   selector: 'app-user-layout',
-  imports: [RouterOutlet, AdminSidebarComponent, HeaderComponent],
+  imports: [RouterOutlet, AdminSidebarComponent, HeaderComponent, UserSidebarComponent],
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.css',
 })
